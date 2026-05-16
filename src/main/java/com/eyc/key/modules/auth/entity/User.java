c6c102137cb9d49695bd4c0d093ad70e0ebe0fb4
duydo <3d1911.doduy@gmail.com>
2026-05-16 13:43:20 +0700

    create entity User

--- src/main/java/com/eyc/key/modules/auth/entity/User.java
+++ src/main/java/com/eyc/key/modules/auth/entity/User.java
@@ -1,10 +1,18 @@
 package com.eyc.key.modules.auth.entity;
 
 import com.eyc.key.common.enums.Role;
+import com.eyc.key.common.enums.UserStatus;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.beans.ConstructorProperties;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -17,7 +25,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID userId ;
@@ -44,6 +52,52 @@ public class User {
     @Column(name = "role", nullable = false)
     private Role role;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private UserStatus status = UserStatus.PENDING_VERIFICATION;
 
+//    số lần đăng nhập sai
+    @Column(name = "failed_login_attempts")
+    @Builder.Default
+    private int failedLoginAttempts = 0;
+//    khóa cho đến lúc nào
+    @Column(name = "locked_until")
+    private LocalDateTime lockedUntil;
+// thời gian tạo user
+    @CreatedDate
+    @Column(name = "craeted_at")
+    private LocalDateTime craetedAt;
+// thời gian chỉnh sủa
+    @LastModifiedDate
+    @Column(name = "update_at")
+    private LocalDateTime updateAt;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("ROLE_"+role.name()));
+    }
+
+    @Override
+    public String getUsername(){
+        return username;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+    @Override
+    public boolean isAccountNonLocked(){
+        if(lockedUntil == null && LocalDateTime.now().isBefore(lockedUntil)){
+            return false;
+        }
+        else {
+            return status != UserStatus.LOCKED && status != UserStatus.SUSPENDED;
+        }
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return status == UserStatus.ACTIVE;
+    }
 }
