fe8087e618e5ff27b321a63a64e30bdd5b8f401a
duydo <3d1911.doduy@gmail.com>
2026-05-18 00:03:31 +0700

    create refreshToken DTO

--- src/main/java/com/eyc/key/modules/auth/dto/request/RefreshToken.java
+++ src/main/java/com/eyc/key/modules/auth/dto/request/RefreshToken.java
@@ -1,4 +1,10 @@
 package com.eyc.key.modules.auth.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class RefreshToken {
+    @NotBlank(message = "Resfresh Không được để trống ")
+    private String refreshToken;
 }
