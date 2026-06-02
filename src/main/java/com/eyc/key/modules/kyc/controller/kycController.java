964ba1cec8997d54f80d578f4c85cc47dcb2000a
Your Name <you@example.com>
2026-06-02 23:28:28 +0700

    create DTO KycSubmitRequest

--- src/main/java/com/eyc/key/modules/kyc/controller/kycController.java
+++ src/main/java/com/eyc/key/modules/kyc/controller/kycController.java
@@ -1,9 +1,11 @@
 package com.eyc.key.modules.kyc.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/kyc")
+@RequiredArgsConstructor
 public class kycController {
 }
