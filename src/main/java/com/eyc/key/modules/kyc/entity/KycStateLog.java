5559597904207703a1e111731bb2ff11a5144ff5
doduy1911 <3d1911.doduy@gmail.com>
2026-06-03 09:15:13 +0700

    pc company

---
+++ src/main/java/com/eyc/key/modules/kyc/entity/KycStateLog.java
@@ -0,0 +1,9 @@
+package com.eyc.key.modules.kyc.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "kyc_state_log")
+public class KycStateLog {
+}
